cc480e4ec2cd51fcd1bd9d26701bdea603f136b3
Ricardo Espinosa <ricardska@gmail.com>
2022-11-14 17:04:59 +0100

    Se Incluye Ribbon

--- DesignManager/Properties/AssemblyInfo.cs
+++ DesignManager/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: NativeResource(14, @"res\C-Comercial.png")]
 [assembly: NativeResource(15, @"res\C-Comercial.png")]
 [assembly: NativeResource(16, @"res\C-Comercial.png")]
-
+[assembly: NativeResource(17, @"res\C-Comercial.png")]
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
