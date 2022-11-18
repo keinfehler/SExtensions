c1d37f229c0f2f2e6eec1a924485cb1b5e75862b
Ricardo Espinosa Jiménez <ricardo.espinosajimenez@bwpackagingsystems.com>
2022-11-18 14:37:58 +0100

    Actualizacion

--- DesignManager/Properties/AssemblyInfo.cs
+++ DesignManager/Properties/AssemblyInfo.cs
@@ -22,6 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: NativeResource(15, @"res\Agrega Pz.png")]
 [assembly: NativeResource(16, @"res\Agrega Con.png")]
 [assembly: NativeResource(17, @"res\Agrega Ch.png")]
+[assembly: NativeResource(18, @"res\Define Sold.png")]
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
