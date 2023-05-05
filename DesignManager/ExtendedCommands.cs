508a4afcfee9e52916445015f379853f5b7e58a6
Ricardo Espinosa Jiménez <ricardo.espinosajimenez@bwpackagingsystems.com>
2023-05-05 14:49:30 +0200

    Intento ReadOnly

--- DesignManager/ExtendedCommands.cs
+++ DesignManager/ExtendedCommands.cs
@@ -54,12 +54,8 @@ namespace DesignManager
 
         internal static void Readonly()
         {
-            //var activeDocument = SolidEdgeAddIn.Instance.Application.ActiveDocument as SolidEdgeDocument;
 
-            //var activeDocumentFullName = activeDocument.FullName;
-            //var activeDocumentDirectoryName = System.IO.Path.GetDirectoryName(activeDocumentFullName);
-
-            //Process.Start(activeDocumentDirectoryName);
+            //    doc.ReadOnly = True
 
 
         }
